147fcd45cb06a677ae6d00eb7b5f36efe2027f2f
Intuzion <intuzion@ubuntu.(none)>
2014-02-07 10:22:52 -0800

    jindal changes

--- db/schema.rb
+++ db/schema.rb
@@ -30,11 +30,6 @@ ActiveRecord::Schema.define(version: 20140206000224) do
     t.datetime "updated_at"
   end
 
-  create_table "userprofiles", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -48,11 +43,6 @@ ActiveRecord::Schema.define(version: 20140206000224) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "gender"
-    t.string   "dob"
-    t.string   "address"
-    t.string   "city"
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
